e5f7f78e77080bd037d2caa409315f789c948d77
FelixMerk <felix@merkonline.com>
2018-04-18 16:16:38 -0400

    changed api layout.  Use curl -d \'{"uid":"1234", "action":"buy", "ticker":"BTC"}\' -H "Content-Type: application/json" http://localhost:5000 to test.

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,21 +1,28 @@
 // Responds to API calls
 module.exports = api
 
+// Requests have the following things
+// 'uid'
+// 'action'
+// 'ticker'
+
 function api(request, response) {
   console.log('success');
   console.log(request.body);      // your JSON
   
   var reply = '';
+
+  // get user from request.body.uid
   
-  if (request.body.MyKey == 'buy') {
+  if (request.body.action == 'buy') {
     console.log('buying stuff');
     //call buy handler
     reply = 'bought stuff\n';
-  } else if (request.body.MyKey == 'sell') {
+  } else if (request.body.action == 'sell') {
     console.log('selling stuff');
     //call sell handler
     reply = 'sold stuff\n';
-  } else if (request.body.MyKey == 'get info') {
+  } else if (request.body.action == 'get info') {
     console.log('informing stuff');
     //send user what stocks they own
     reply = 'informed you\n';
