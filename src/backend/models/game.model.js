1994bd665d73e92332011d6e8fc80b00fff05f24
rladbsgh <therladbsgh@gmail.com>
2018-04-18 15:14:57 -0400

    Switch to connect mongo

--- src/backend/models/game.model.js
+++ src/backend/models/game.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const GameSchema = new Schema({
+  gameid: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
