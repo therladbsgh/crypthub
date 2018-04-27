8685845d9d5dba93bed63f969e70e6e239101fa1
rladbsgh <therladbsgh@gmail.com>
2018-04-26 21:48:53 -0400

    Add /coin/getall route

---
+++ src/backend/controllers/coin.controller.js
@@ -0,0 +1,11 @@
+const Coin = require('../models/coin.model');
+
+function getAllCoin(req, res) {
+  Coin.find({ symbol: { $ne: 'USD' }}).exec().then((coins) => {
+    res.status(200).json({ data: coins });
+  });
+}
+
+module.exports = {
+  getAllCoin
+}
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const userRoutes = require('./user.routes');
 const sessionRoutes = require('./session.routes');
 const gameRoutes = require('./game.routes');
+const coinRoutes = require('./coin.routes');
 
 
 const router = Router();
@@ -13,5 +14,6 @@ router.use('/ping', (req, res) => {
 router.use('/passport', sessionRoutes);
 router.use('/users', userRoutes);
 router.use('/game', gameRoutes);
+router.use('/coin', coinRoutes);
 
 module.exports = router;
