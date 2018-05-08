680af9dcd14556f984cc34c47984fc9f063cb09c
Benjamin Wesner <bewesner1@gmail.com>
2018-05-08 16:06:52 -0400

    frontend tests

--- src/frontend/components/PageNotFound/__tests__/PageNotFound.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PageNotFound from '../PageNotFound';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  	const tree = renderer.create(<PageNotFound />).toJSON();
-  	expect(tree).toMatchSnapshot();
-});
